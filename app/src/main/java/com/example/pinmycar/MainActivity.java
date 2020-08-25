9f8ca2d6b3fd8ef822c62d1890f04953fbed1893
Laurene Flamey <laurene.flamey@live.fr>
2020-08-25 18:28:52 +0200

    get geolocation ok

--- app/src/main/java/com/example/pinmycar/MainActivity.java
+++ app/src/main/java/com/example/pinmycar/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.pinmycar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private int locationRequestCode = 1000;
+    private double latitude = 0.0, longitude = 0.0;
+
+    private TextView txtLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.txtLocation = (TextView) findViewById(R.id.txtLocation);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // check permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    locationRequestCode);
+        } else {
+            // already permission granted
+        }
+
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                            txtLocation.setText(String.format(Locale.FRANCE, "%s , %s", latitude, longitude));
+                        } else {
+                            txtLocation.setText(String.format("fail"));
+                        }
+                    }
+                });
     }
 }
